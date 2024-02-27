5db7ad305f53bc01e2b8ed7d06cef311732fc179
Simon Hakvoort <s.m.a.hakvoort@students.uu.nl>
2024-02-27 17:12:21 +0000

    Set initial values for trunc normal and gev

--- src/models/initial_params.py
+++ src/models/initial_params.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def get_gev_initial_params():
     output = {
-        'a_gev' : np.array([0.4716438]),
-        'b_gev' : np.array([ 0.9455215,  -0.09072546, -0.10659732,  0.03168057,  0.09318347]),
-        'c_gev' : np.array([0.00019721]),
-        'd_gev' : np.array([-3.96872820e-05, -9.66233783e-05,  5.97309445e-05, -1.32532659e-04, 1.04264735e-04]),
-        'e_gev' : np.array([0.51883376])
+        'a_gev' : np.array([0.7259205]),
+        'b_gev' : np.array([ 0.95463854, -0.15831928, -0.20902145, -0.01082994,  0.11957027]),
+        'c_gev' : np.array([-0.08465461]),
+        'd_gev' : np.array([-0.01219114,  0.02037963,  0.05282509,  0.00752834,  0.00600949]),
+        'e_gev' : np.array([0.6217914])
     }
     return output
 
@@ -14,7 +14,8 @@ def get_trunc_normal_initial_params():
     output = {
         'a_tn' : np.array([0.4743731]),
         'b_tn' : np.array([ 0.9456868,  -0.0903203,  -0.10497134,  0.02873755,  0.09236028]),
-        'c_tn' : np.array([[0.0002737]]),
-        'd_tn' : np.array([-0.00017721])
+        'c_tn' : np.array([0.73862517]),
+        'd_tn' : np.array([0.4235527])
     }
     return output
+
