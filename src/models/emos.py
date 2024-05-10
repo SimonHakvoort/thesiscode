Fixed a bug in loading of EMOS models. Add a different form of normalization

src/models/emos.py
@@ -141,7 +141,8 @@ class EMOS:
         try:
             if 'learning_rate' not in setup:
                 raise ValueError("Learning rate not specified")
-            self.optimizer = getattr(tf.optimizers, setup['optimizer'])(learning_rate=setup['learning_rate'])
+            learning_rate = round(float(setup['learning_rate']), 6)
+            self.optimizer = getattr(tf.optimizers, setup['optimizer'])(learning_rate=learning_rate)
         except AttributeError:
             raise ValueError("Invalid optimizer: " + setup['optimizer']) 
         

src/models/selection.py
@@ -73,13 +73,13 @@ setup = {'loss': loss,
 
 
 neighbourhood_size = 11
-epochs = 100
+epochs = 20
 test_fold = 3
 folds = [1,2]
 ignore = ['229', '285', '323']
 
 # tf.debugging.enable_check_numerics()
-folder = '/net/pc200239/nobackup/users/hakvoort/models/emos/'
+folder = '/net/pc200239/nobackup/users/hakvoort/models/emos/test'
 
 # time the length that train_emos takes
 start = time.time()
@@ -96,7 +96,11 @@ X_test = (X_test - model.feature_mean) / model.feature_std
 
 print(model.CRPS(X_test, y_test, 20000))
 
-print(model.Brier_Score(X_test, y_test, 10))
-print(model.Brier_Score(X_test, y_test, 15))
+mydict = model.to_dict()
 
-print(model.twCRPS(X_test, y_test, 12, 1000))
+#pickle mydict to folder
+
+with open(folder, 'wb') as f:
+    pkl.dump(mydict, f)
+
+print("Model saved")

src/models/test.py
@@ -1,70 +1,89 @@
-import pickle as pkl
 import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
-
-from src.models.train_emos import train_emos, train_and_test_emos
-from src.training.training import load_model, train_and_save
-from src.visualization.brier_score import make_brier_skill_plot, brier_plot
-from src.visualization.pit import make_cpit_diagram_emos
-from src.visualization.plot_forecasts import plot_forecast_pdf
-from src.visualization.reliability_diagram import make_reliability_diagram
-from src.models.get_data import get_tensors, get_normalized_tensor
+from src.models.train_emos import train_emos
 from src.models.emos import EMOS
-from src.models.emos import EMOS
+from src.models.get_data import get_tensors
+from src.models.train_emos import train_and_test_emos
+from src.training.training import load_model
+from src.visualization.brier_score import make_brier_skill_plot
+from src.visualization.pit import make_cpit_hist_emos
+from src.visualization.reliability_diagram import make_reliability_and_sharpness
+from src.visualization.scoring_tables import make_table
+from src.models.probability_distributions import TruncGEV
+import pickle as pkl
+import time
 
-forecast_distribution = 'distr_mixture_linear'
-distribution_1 = 'distr_trunc_normal'
-distribution_2 = 'distr_gev'
-
-loss = 'loss_twCRPS_sample' # options: loss_CRPS_sample, loss_twCRPS_sample, loss_log_likelihood
-
-chain_function = 'chain_function_normal_cdf' # options: chain_function_normal_cdf, chain_function_indicator, chain_function_normal_cdf_plus_constant
-chain_function_mean = 13
-chain_function_std = 2
-chain_function_threshold = 15 # 12 / 15
-chain_function_constant = 0.02
-
-optimizer = 'Adam'
-learning_rate = 0.03
-folds = [1,2]
-neighbourhood_size = 11
-ignore = ['229', '285', '323']
-epochs = 600
-
-samples = 100
-printing = True
-pretrained = True
-random_init = False
 
 all_features = ['wind_speed', 'press', 'kinetic', 'humid', 'geopot', 'spatial_variance']
 
 location_features = ['wind_speed', 'press', 'kinetic', 'humid', 'geopot']
 
-scale_features = ['spatial_variance']
+scale_features = ['wind_speed', 'press', 'kinetic', 'humid', 'geopot']
 
-model = train_and_save(
-    forecast_distribution,
-    loss,
-    optimizer,
-    learning_rate,
-    folds,
-    all_features,
-    location_features,
-    scale_features,
-    neighbourhood_size,
-    ignore,
-    epochs,
 
-    chain_function = chain_function,
-    chain_function_mean = chain_function_mean,
-    chain_function_std = chain_function_std,
-    chain_function_constant = chain_function_constant,
-    chain_function_threshold = chain_function_threshold,
-    samples = samples,
-    printing = printing,
-    distribution_1 = distribution_1,
-    distribution_2 = distribution_2,
-    pretrained = pretrained,
-    random_init = random_init
-)
+
+
+# possible loss functions: 'loss_CRPS_sample', 'loss_log_likelihood', 'loss_Brier_score', 'loss_twCRPS_sample'
+loss = "loss_CRPS_sample"
+samples = 100
+
+# possible chain functions: 'chain_function_indicator' and 'chain_function_normal_cdf'
+# if chain_function_indicator is chosen, threshold is not necessary
+# if chain_function_normal_cdf is chosen, threshold is necessary
+chain_function = "chain_function_normal_cdf_plus_constant"
+threshold = 8
+chain_function_mean = 13
+chain_function_std = 2
+chain_function_constant = 0.07
+
+
+# possible optimizers: 'SGD', 'Adam'
+optimizer = "Adam"
+learning_rate = 0.05
+
+# possible forecast distributions: 'distr_trunc_normal', 'distr_log_normal', 'distr_gev' and 'distr_mixture'/'distr_mixture_linear', which can be a mixture distribution of two previously mentioned distributions.
+forecast_distribution = "distr_trunc_normal"
+
+# necessary in case of a mixture distribution
+distribution_1 = "distr_trunc_normal"
+distribution_2 = "distr_log_normal"
+
+random_init = False
+printing = False
+subset_size = None
+
+setup = {'loss': loss,
+         'samples': samples, 
+         'optimizer': optimizer, 
+         'learning_rate': learning_rate, 
+         'forecast_distribution': forecast_distribution,
+         'chain_function': chain_function,
+         'threshold': threshold,
+         'distribution_1': distribution_1,
+         'distribution_2': distribution_2,
+         'chain_function_mean': chain_function_mean,
+         'chain_function_std': chain_function_std,
+         'chain_function_constant': chain_function_constant,
+         'location_features': location_features,
+         'scale_features': scale_features,
+         'random_init': random_init,
+         'subset_size': subset_size,
+        'printing': printing,
+         }
+
+
+neighbourhood_size = 11
+epochs = 20
+test_fold = 3
+folds = [1,2]
+ignore = ['229', '285', '323']
+
+# tf.debugging.enable_check_numerics()
+folder = '/net/pc200239/nobackup/users/hakvoort/models/emos/test'
+
+# time the length that train_emos takes
+start = time.time()
+
+model = load_model(folder)
+
+x = 3

src/neural_networks/get_data.py
@@ -4,7 +4,7 @@ import pdb
 
 from src.models.get_data import get_fold_i, get_station_info
 
-def get_tf_data(fold, feature_names, ignore = []):
+def get_tf_data(fold, feature_names, ignore = [], add_emos = False):
     """
     Gets a specific fold number and feature names and returns the data as a tf.data.Dataset
 
@@ -41,9 +41,27 @@ def get_tf_data(fold, feature_names, ignore = []):
     X = {key: tf.convert_to_tensor(value) for key, value in X.items()}
     y = tf.convert_to_tensor(y_list)
 
+    if add_emos:
+        # Add a key 'features_emos' to X that contains all the features_names that are 1D, and the centre grid cell of wind_speed_grid
+        temp = {}
+        for feature in feature_names:
+            if feature_names[feature] == 1:
+                temp[feature] = X[feature]
+            else:
+                name = feature + '_grid'
+                temp[feature] = X[name][:, feature_names[feature] // 2, feature_names[feature] // 2]
+        X['features_emos'] = tf.stack([temp[feature] for feature in temp], axis=1)
+
+        mean = tf.reduce_mean(X['features_emos'], axis=0)
+        std = tf.math.reduce_std(X['features_emos'], axis=0)
+
+        X['features_emos'] = (X['features_emos'] - mean) / std
+
     # To ensure that the wind_speed_grid is a 3D tensor, where the final dimension is for the number of channels.
     if 'wind_speed_grid' in X:
         X['wind_speed_grid'] = tf.expand_dims(X['wind_speed_grid'], axis=-1)
+
+
 
     data = tf.data.Dataset.from_tensor_slices((X, y))
 