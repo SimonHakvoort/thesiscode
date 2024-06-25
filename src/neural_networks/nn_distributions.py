Fixed the distribution for the log normal in the CNN model, such that the actual mean and variance are computed for the LN

src/neural_networks/nn_distributions.py
@@ -63,15 +63,6 @@ class NNDistribution(ABC):
     def get_config(self):
         return {'name': self.__str__()}
 
-    # def save(self, filepath):
-    #     with open(filepath, 'wb') as f:
-    #         pickle.dump(self, f)
-
-    # @staticmethod
-    # def load(filepath):
-    #     with open(filepath, 'rb') as f:
-    #         return pickle.load(f)
-
     
 @register_keras_serializable(package='Custom')
 class NNTruncNormal(NNDistribution):
@@ -108,7 +99,13 @@ class NNLogNormal(NNDistribution):
     def get_distribution(self, y_pred):
         loc = y_pred[:, 0]
         scale = y_pred[:, 1]
-        return tfp.distributions.LogNormal(loc=loc, scale=scale)
+
+        mean = tf.math.log(loc ** 2) - 0.5 * tf.math.log(scale + loc ** 2)
+
+        # We add a small value to avoid numerical instability
+        sigma = tf.sqrt(tf.math.log(1 + scale / loc ** 2) + 1e-6)
+
+        return tfp.distributions.LogNormal(loc=mean, scale=sigma)
     
     def build_output_layers(self):
         mu = Dense(1, activation='linear')

src/neural_networks/test_nn.py
@@ -20,160 +20,166 @@ features_names_dict['wind_speed'] = 15
 
 ignore = ['229', '285', '323']
 
-save_cv_data(features_names_dict, ignore)
+# save_cv_data(features_names_dict, ignore)
 
-# train_data, test_data, data_info = load_cv_data(3, features_names_dict)
+train_data, test_data, data_info = load_cv_data(3, features_names_dict)
 
-# train_data = train_data.shuffle(len(train_data))
+train_data = train_data.shuffle(len(train_data))
 
-# train_data = train_data.batch(32)
+train_data = train_data.batch(32)
 
-# train_data = train_data.prefetch(tf.data.experimental.AUTOTUNE)
+train_data = train_data.prefetch(tf.data.experimental.AUTOTUNE)
 
-# test_data = test_data.batch(len(test_data))
+test_data = test_data.batch(len(test_data))
 
-# test_data = test_data.prefetch(tf.data.experimental.AUTOTUNE)
+test_data = test_data.prefetch(tf.data.experimental.AUTOTUNE)
 
-# forecast_distribution = 'distr_trunc_normal'
-# distribution_1 = 'distr_trunc_normal'
-# distribution_2 = 'distr_log_normal'
+forecast_distribution = 'distr_gev'
+distribution_1 = 'distr_trunc_normal'
+distribution_2 = 'distr_log_normal'
 
-# loss_function = 'loss_twCRPS_sample'
-# chain_function = 'chain_function_normal_cdf_plus_constant'
-# chain_function_mean = 10
-# chain_function_std = 0.01
-# chain_function_constant = 0.001
+loss_function = 'loss_CRPS_sample'
+chain_function = 'chain_function_normal_cdf_plus_constant'
+chain_function_mean = 10
+chain_function_std = 0.01
+chain_function_constant = 0.001
 
-# optimizer = 'adam'
-# learning_rate = 0.0002
-
-
-# dense_l1_regularization = 0.000
-# dense_l2_regularization = 0.0003
-# hidden_units_list = [100, 100, 100]
-# add_nwp_forecast = True
-# conv_7x7_units = 5
-# conv_5x5_units = 5
-# conv_3x3_units = 5
-# add_wind_conv = True
-
-# metrics = ['twCRPS_12']# ['twCRPS_10', 'twCRPS_12', 'twCRPS_15']
-# metrics = None
-# saving = True
-
-# epochs = 80
-
-# filepath = '/net/pc200239/nobackup/users/hakvoort/models/conv_nn/'
-
-# if loss_function == 'loss_twCRPS_sample':
-#     name = 'twCRPS'
-#     name += '_mean_' + str(chain_function_mean)
-#     name += '_std_' + str(chain_function_std)
-#     name += '_constant_' + str(chain_function_constant)
-# elif loss_function == 'loss_CRPS_sample':
-#     name = 'CRPS'
-
-# filepath += name + '_'
-
-# if forecast_distribution == 'distr_mixture':
-#     filepath += 'mixture_'
-# elif forecast_distribution == 'distr_trunc_normal':
-#     filepath += 'trunc_normal_'
-# elif forecast_distribution == 'distr_log_normal':
-#     filepath += 'log_normal_'
-
-# filepath += 'epochs_' + str(epochs) 
+optimizer = 'adam'
+learning_rate = 0.0005
 
 
+dense_l1_regularization = 0.000
+dense_l2_regularization = 0.0003
+hidden_units_list = [100, 100, 100]
+add_nwp_forecast = True
+conv_7x7_units = 4
+conv_5x5_units = 4
+conv_3x3_units = 4
+add_wind_conv = True
 
-# filepath += '_early_stopping_1'
+metrics = ['twCRPS_12']# ['twCRPS_10', 'twCRPS_12', 'twCRPS_15']
+metrics = None
+saving = False
+
+epochs = 80
+
+filepath = '/net/pc200239/nobackup/users/hakvoort/models/conv_nn/'
+
+if loss_function == 'loss_twCRPS_sample':
+    name = 'twCRPS'
+    name += '_mean_' + str(chain_function_mean)
+    name += '_std_' + str(chain_function_std)
+    name += '_constant_' + str(chain_function_constant)
+elif loss_function == 'loss_CRPS_sample':
+    name = 'CRPS'
+
+filepath += name + '_'
+
+if forecast_distribution == 'distr_mixture':
+    filepath += 'mixture_'
+elif forecast_distribution == 'distr_trunc_normal':
+    filepath += 'trunc_normal_'
+elif forecast_distribution == 'distr_log_normal':
+    filepath += 'log_normal_'
+
+filepath += 'epochs_' + str(epochs) 
 
 
-# # make a folder
-# if saving:
-#     os.makedirs(filepath, exist_ok=True)
 
-# setup_distribution = {
-#     'forecast_distribution': forecast_distribution,
-#     'distribution_1': distribution_1,
-#     'distribution_2': distribution_2,
-# }
+filepath += '_early_stopping_1'
 
-# setup_nn_architecture = {
-#     'hidden_units_list': hidden_units_list,
-#     'dense_l1_regularization': dense_l1_regularization,
-#     'dense_l2_regularization': dense_l2_regularization,
-#     'add_nwp_forecast': add_nwp_forecast,
 
-#     'conv_7x7_units': conv_7x7_units,
-#     'conv_5x5_units': conv_5x5_units,
-#     'conv_3x3_units': conv_3x3_units,
-# }
+# make a folder
+if saving:
+    os.makedirs(filepath, exist_ok=True)
 
-# setup_loss = {
-#     'loss_function': loss_function,
-#     'chain_function': chain_function,
-#     'chain_function_mean': chain_function_mean,
-#     'chain_function_std': chain_function_std,
-#     'chain_function_constant': chain_function_constant,
-# }
+setup_distribution = {
+    'forecast_distribution': forecast_distribution,
+    'distribution_1': distribution_1,
+    'distribution_2': distribution_2,
+}
 
-# setup_optimizer = {
-#     'optimizer': optimizer,
-#     'learning_rate': learning_rate,
-# }
+setup_nn_architecture = {
+    'hidden_units_list': hidden_units_list,
+    'dense_l1_regularization': dense_l1_regularization,
+    'dense_l2_regularization': dense_l2_regularization,
+    'add_nwp_forecast': add_nwp_forecast,
 
-# setup = {
-#     'setup_distribution': setup_distribution,
-#     'features_names': features_names,
-#     'setup_loss': setup_loss,
-#     'setup_optimizer': setup_optimizer,
-#     'sample_size': 100,
-#     'setup_nn_architecture': setup_nn_architecture,
+    'conv_7x7_units': conv_7x7_units,
+    'conv_5x5_units': conv_5x5_units,
+    'conv_3x3_units': conv_3x3_units,
+}
 
-#     'add_wind_conv': add_wind_conv,
+setup_loss = {
+    'loss_function': loss_function,
+    'chain_function': chain_function,
+    'chain_function_mean': chain_function_mean,
+    'chain_function_std': chain_function_std,
+    'chain_function_constant': chain_function_constant,
+}
 
-#     'features_1d_mean': data_info['features_1d_mean'],
-#     'features_1d_std': data_info['features_1d_std'],
+setup_optimizer = {
+    'optimizer': optimizer,
+    'learning_rate': learning_rate,
+}
 
-#     'metrics': metrics,
-# }
+setup = {
+    'setup_distribution': setup_distribution,
+    'features_names': features_names,
+    'setup_loss': setup_loss,
+    'setup_optimizer': setup_optimizer,
+    'sample_size': 100,
+    'setup_nn_architecture': setup_nn_architecture,
 
-# if saving:
-#     with open(filepath + '/attributes', 'wb') as f:
-#         pickle.dump(setup, f)
+    'add_wind_conv': add_wind_conv,
 
-# nn = NNForecast(**setup)
+    'features_1d_mean': data_info['features_1d_mean'],
+    'features_1d_std': data_info['features_1d_std'],
 
-# #start the time
-# time_start = time.time()
+    'metrics': metrics,
+}
 
-# early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+if saving:
+    with open(filepath + '/attributes', 'wb') as f:
+        pickle.dump(setup, f)
 
-# history = nn.fit(train_data, epochs=epochs, validation_data=test_data , early_stopping=early_stopping)
+nn = NNForecast(**setup)
 
-# best_epoch = early_stopping.stopped_epoch - early_stopping.patience
+#start the time
+time_start = time.time()
 
-# print(f'Best epoch: {best_epoch}')
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
-# #end the time
-# time_end = time.time()
+history = nn.fit(train_data, epochs=epochs, validation_data=test_data , early_stopping=early_stopping)
 
-# print("Time: ", time_end - time_start)
+best_epoch = early_stopping.stopped_epoch - early_stopping.patience
 
-# print(nn.CRPS(test_data, 10000).numpy())
+print(f'Best epoch: {best_epoch}')
 
-# print(nn.twCRPS(test_data, [12], 10000)[0].numpy())
+#end the time
+time_end = time.time()
 
-# if saving:
-#     nn.save_weights(filepath)
-#     print("Model saved")
+print("Time: ", time_end - time_start)
 
-# # save the history
-# if saving:
-#     with open(filepath + '/history.pickle', 'wb') as f:
-#         pickle.dump(history.history, f)
-#         print("History saved")
+print(nn.CRPS(test_data, 10000).numpy())
+
+print(nn.twCRPS(test_data, [12], 10000)[0].numpy())
+
+values = np.linspace(0, 20, 40)
+
+brierscores = nn.Brier_Score(test_data, values)
+
+print(brierscores)
+
+if saving:
+    nn.save_weights(filepath)
+    print("Model saved")
+
+# save the history
+if saving:
+    with open(filepath + '/history.pickle', 'wb') as f:
+        pickle.dump(history.history, f)
+        print("History saved")
 
 
 